e9e6f6947a0fab0958574c75bcf89ee0252496b2
imine25 <ialluu@88.com>
2025-11-22 14:28:07 +0800

    6633

--- tmarks/functions/api/tab/search.ts
+++ tmarks/functions/api/tab/search.ts
@@ -5,9 +5,9 @@
  */
 
 import type { PagesFunction } from '@cloudflare/workers-types'
-import type { Env, Bookmark, RouteParams } from '../../../lib/types'
-import { success, badRequest, internalError } from '../../../lib/response'
-import { requireApiKeyAuth, ApiKeyAuthContext } from '../../../middleware/api-key-auth-pages'
+import type { Env, Bookmark, RouteParams } from '../../lib/types'
+import { success, badRequest, internalError } from '../../lib/response'
+import { requireApiKeyAuth, ApiKeyAuthContext } from '../../middleware/api-key-auth-pages'
 
 // GET /api/search - 全局搜索书签和标签
 type BookmarkWithTags = Bookmark & {
